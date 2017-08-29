ecfbe305c22a33932212c1f1b3a6ce1de9770212
BlazingFire007 <elijahrich02@icloud.com>
2017-08-29 15:11:55 -0500

    Update template.js

--- template.js
+++ template.js
@@ -1,8 +1,14 @@
-function templateIfy(str,t,obj=false) {
-  if (t<1) return;
-  let res="";
-  for(let i=0; i<t; i++) res+="`${";
-  obj?res+=str:res+="`"+str+"`";
-  for(let i=0; i<t; i++)res+="}`";
-  return res;
+window.run = function() {
+  function templateIfy(str,t,obj=false) {
+    if (t<1) return;
+    let res="";
+    for(let i=0; i<t; i++) res+="`${";
+    obj?res+=str:res+="`"+str+"`";
+    for(let i=0; i<t; i++)res+="}`";
+    return res;
+  }
+  $=s=>document.getElementById(s);
+  $("res").innerHTML=templateIfy($("code").value,$("n").value);
+  $("res").style='';
+  $("cp").style='';
 }
